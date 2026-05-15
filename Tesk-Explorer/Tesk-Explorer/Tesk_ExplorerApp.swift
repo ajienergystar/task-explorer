a1e93fb3b02f83ff95e77fed49fa2a2aef473d3d
Aji Prakosa <aji.prakosa@feedloop.ai>
2026-05-15 08:35:41 +0700

    Initial commit

---
+++ Tesk-Explorer/Tesk-Explorer/Tesk_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesk_ExplorerApp.swift
+//  Tesk-Explorer
+//
+//  Created by Aji Prakosa on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Tesk_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tesk-Explorer/Tesk-ExplorerTests/Tesk_ExplorerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Tesk_ExplorerTests.swift
+//  Tesk-ExplorerTests
+//
+//  Created by Aji Prakosa on 15/05/26.
+//
+
+import Testing
+
+struct Tesk_ExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
